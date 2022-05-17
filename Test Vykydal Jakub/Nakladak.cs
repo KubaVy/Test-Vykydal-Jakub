8ec81ef41aa15b26afea4a770eeb4d0e8cd76668
Jakub Vykydal <Vykydal.Jakub@stud.vassboskovice.cz>
2022-05-17 11:02:20 +0200

    jhjk

--- Test Vykydal Jakub/Nakladak.cs
+++ Test Vykydal Jakub/Nakladak.cs
@@ -6,13 +6,31 @@ namespace Test_Vykydal_Jakub
 {
     class Nakladak
     {
-        public int maxNosnost { get; }
-        public int maxPalivo { get; }
-        public int spotreba { get; }
+        
+        
+        public int maxNosnost { get; set; }
+        public int maxPalivo { get; set; }
+        public int spotreba { get; set; }
+        public int vzdalenost { get; set; }
         public int palivo { get; set; }
         public int naklad { get; set; }
-    
 
+        public Nakladak()
+        {
+            int maxNosnost = 3000;
+            int maxpalivo = 1500;
+            int spotreba = 50;
+            int naklad = 0;
+        }
+        public void Nalozit()
+        {
+            int naklad =+ 1000;
+            if (maxNosnost == 3000)
+            {
+                
+            }
+        }
+    
     
     }
 }
