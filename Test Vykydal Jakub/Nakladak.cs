12a3fe6708391293d25fdfe9543f86a69364b6d6
Jakub Vykydal <Vykydal.Jakub@stud.vassboskovice.cz>
2022-05-17 10:33:38 +0200

    Add project files.

---
+++ Test Vykydal Jakub/Nakladak.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Vykydal_Jakub
+{
+    class Nakladak
+    {
+        public int maxNosnost { get; }
+        public int maxPalivo { get; }
+        public int spotreba { get; }
+        public int palivo { get; set; }
+        public int naklad { get; set; }
+    
+
+    
+    }
+}
